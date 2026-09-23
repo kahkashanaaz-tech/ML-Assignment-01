5ef64d6e509a322b6b2ea8d4bc739b73a6436ebc
Kahkasha Naaz <kahkashanaaz2005@gmail.com>
2026-09-23 14:01:58 +0530

    (03)student_dataframe.py

--- Machine Learning-Assignment01/Experiment 1(ML)/(03)student_dataframe.py
+++ Machine Learning-Assignment01/Experiment 1(ML)/(03)student_dataframe.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-# Create a DataFrame
 data = {
     "Student Name": ["Amit", "Riya", "Sourav", "Neha", "Rahul"],
     "Roll Number": [101, 102, 103, 104, 105],
@@ -9,14 +8,9 @@ data = {
 }
 
 df = pd.DataFrame(data)
-
-# Display the complete DataFrame
 print("Complete Student Records:")
 print(df)
-
-# Filter students who scored above 80 marks
 filtered_df = df[df["Marks"] > 80]
 
-# Display filtered records
 print("\nStudents who scored above 80 marks:")
 print(filtered_df)
